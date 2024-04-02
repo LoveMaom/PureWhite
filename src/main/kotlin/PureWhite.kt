d28e605575605418f507e3113c6045db5fe1e194
Love_Maom <1700721266@qq.com>
2024-04-02 10:02:36 +0800

    更新 不需要在data下面添加图片也能使用

--- src/main/kotlin/PureWhite.kt
+++ src/main/kotlin/PureWhite.kt
@@ -1,7 +1,17 @@
 package com.purewhite
 
+import com.purewhite.plugin.common.Create
+import com.purewhite.plugin.config.FuckAdminConfig
+import com.purewhite.plugin.config.FuckManagementConfig
+import com.purewhite.plugin.config.FuckMemberConfig
+import com.purewhite.plugin.config.MessageConfig
+import com.purewhite.plugin.translate.FuckAdmin
+import com.purewhite.plugin.translate.FuckManagement
+import com.purewhite.plugin.translate.FuckMember
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+import net.mamoe.mirai.event.GlobalEventChannel
+import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.utils.info
 
 object PureWhite : KotlinPlugin(
@@ -15,6 +25,17 @@ object PureWhite : KotlinPlugin(
     }
 ) {
     override fun onEnable() {
-        logger.info { "Plugin loaded" }
+        logger.info { "噼里啪啦,最弱插件启动成功" }
+        // 创建文件夹
+        Create.createFolder("data/com.purewhite.entertainment/FuckNo")
+        FuckMemberConfig.reload()
+        FuckAdminConfig.reload()
+        FuckManagementConfig.reload()
+        MessageConfig.reload()
+        GlobalEventChannel.subscribeAlways<GroupMessageEvent> {
+            FuckMember.main(this)
+            FuckAdmin.main(this)
+            FuckManagement.main(this)
+        }
     }
 }
